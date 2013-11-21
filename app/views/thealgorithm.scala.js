d70bd1b3d1888a595b70cd226ebe3fb2628e14f7
Nick Childers <nick@jobhive.com>
2013-11-21 10:40:24 -0800

    Zoundz

--- app/views/thealgorithm.scala.js
+++ app/views/thealgorithm.scala.js
@@ -1,14 +1,24 @@
 @(char_id: String)(implicit req: RequestHeader)
+
 $(function() {
+
+  buzz.defaults.formats = [ 'ogg', 'mp3' ];
+
+  var elephant = new buzz.sound("@routes.Assets.at("sounds/elephant")");
+  var soundHA = new buzz.sound("@routes.Assets.at("sounds/elephant")");
+  var soundMEDIC = new buzz.sound("@routes.Assets.at("sounds/elephant")");
+  var soundLA = new buzz.sound("@routes.Assets.at("sounds/elephant")");
+  var soundENGY = new buzz.sound("@routes.Assets.at("sounds/elephant")");
+  var soundINF = new buzz.sound("@routes.Assets.at("sounds/elephant")");
+
   console.log(jsRoutes);
+
   var WS = window['MozWebSocket'] ? MozWebSocket : WebSocket
   var algosocket = new WS("@routes.Application.thealgorithm.webSocketURL()")
 
   var receiveEvent = function(event) {
       var wat = $.parseJSON(event.data)
-      console.log(wat);
-      console.log(wat);
-      console.log(wat);
+      console.log("EVENT -- " + wat);
       $.get("@routes.Application.squadInfo(char_id)",function(data) {
           $(".jumbotron").html(" " +
               "<h2>Your Default Role (Be this class): <b>"+data["role"]+"</b></h2>" +
@@ -31,6 +41,11 @@ $(function() {
       }
       if(wat.role_change == "@char_id") {
         console.log(wat.role);
+        if(wat.role == "Heavy Assault") { soundHA.play() }
+        if(wat.role == "Medic") { soundMEDIC.play() }
+        if(wat.role == "Light Assault") { soundLA.play() }
+        if(wat.role == "Engineer") { soundENGY.play() }
+        if(wat.role == "Infiltrator") { soundINF.play() }
       }
   }
 
