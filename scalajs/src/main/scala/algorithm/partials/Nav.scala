d63e36c503d7f22c2a1be7deeb4e1d7a20898426
Nick <nick@stabtechs.com>
2014-08-30 11:11:20 -0700

    Added locale

--- scalajs/src/main/scala/algorithm/partials/Nav.scala
+++ scalajs/src/main/scala/algorithm/partials/Nav.scala
@@ -1,8 +1,14 @@
 package algorithm.partials
 
+import algorithm.AlgorithmJS
+import org.scalajs.dom
+import org.scalajs.dom.HTMLSelectElement
+
+import scala.scalajs.js
 import scalatags.JsDom.all._
 import shared.models._
 import rx._
+import algorithm.framework.Framework._
 
 trait LocalLink
 case object SquadLink extends LocalLink
@@ -14,6 +20,40 @@ case object VoiceTestLink extends LocalLink
 object Nav {
 
   val currentLink: Var[Option[LocalLink]] = Var(None)
+
+  val selectedLocale: Var[String] = Var("en-US")
+
+  val englishLocale = List(
+    ("en-AU", "Australia"),
+    ("en-CA", "Canada"),
+    ("en-IN", "India"),
+    ("en-NZ", "New Zealand"),
+    ("en-ZA", "South Africa"),
+    ("en-GB", "United Kingdom"),
+    ("en-US", "United States")
+  )
+
+  def localeChanged: js.ThisFunction0[HTMLSelectElement,Boolean] = { (select: HTMLSelectElement) =>
+    selectedLocale() = select.value
+    if(AlgorithmJS.annyang.isInstanceOf[js.Object]) {
+      AlgorithmJS.annyang.setLanguage(select.value)
+    }
+    true
+  }
+
+  val selectLangLocale: Rx[HtmlTag] = Rx {
+    select(
+      onchange := localeChanged,
+      englishLocale.map { case (ident,description) =>
+        val isSelected = selectedLocale() == ident
+        option(
+          description,
+          value:=ident,
+          if(isSelected) "selected".attr:="selected" else ()
+        )
+      }
+    )
+  }
 
   def linkTo(link: LocalLink, text: String): HtmlTag = {
     a(
@@ -33,7 +73,8 @@ object Nav {
             li(linkTo(PreferenceLink,"Preferences")),
             li(linkTo(CreatePatternLink,"Create Pattern")),
             li(linkTo(VoiceTestLink,"Voice Command Test")),
-            li(linkTo(SquadLink,"Main"))
+            li(linkTo(SquadLink,"Main")),
+            li(a(href:="#")(selectLangLocale))
           )
         )
       )
